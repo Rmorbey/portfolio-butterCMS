a62f8559f2bd7c68319dea28d632573375b8d0f7
Russell <russellmorbey@hotmail.co.uk>
2025-07-04 13:51:13 +0100

    Navbar comp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+	FaBars,
+	FaTimes,
+	FaGithub,
+	FaLinkedin,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
-function Navbar() {
+const Navbar = () => {
+	const [nav, setNav] = useState(false);
+	const handleClick = () => setNav(!nav);
+
 	return (
-		<div>Navbar</div>
+		<div className='fixed w-full h-20 flex justify-between items-center px-4 bg-slate-900 text-gray-300'>
+			<div>
+				<h1 className='font-thin text-2xl italic font-serif'>RM</h1>
+			</div>
+
+			<ul className='hidden md:flex gap-x-8'>
+				<li>
+					<Link to='home' smooth={true} duration={500}>
+					Home
+					</Link>
+				</li>
+				<li>
+					<Link to='about' smooth={true} duration={500}>
+					About
+					</Link>
+				</li>
+				<li>
+					<Link to='skills' smooth={true} duration={500}>
+					Skills
+					</Link>
+				</li>
+				<li>
+					<Link to='works' smooth={true} duration={500}>
+					Works
+					</Link>
+				</li>
+				<li>
+					<Link to='contact' smooth={true} duration={500}>
+					Contact
+					</Link>
+				</li>
+			</ul>
+
+			<div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
+				{!nav ? <FaBars size={20} /> : <FaTimes size={20} />}
+			</div>
+			<ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-slate-900 flex flex-col justify-center items-center'}>
+				<li className='py-6 text-4xl'>
+					<Link onClick={handleClick} to='home' smooth={true} duration={500}>
+					Home
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link onClick={handleClick} to='about' smooth={true} duration={500}>
+					About
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+					Skills
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link onClick={handleClick} to='works' smooth={true} duration={500}>
+					Works
+					</Link>
+				</li>
+				<li className='py-6 text-4xl'>
+					<Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+					Contact
+					</Link>
+				</li>
+			</ul>
+
+			<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+				<ul>
+					<li classNames='w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+						<a href="https://linkedin.com/in/russell-morbey-706063246" className='flex justify-between items-center w-full text-gray-300 px-4'>
+						LinkedIn <FaLinkedin size={30} />
+						</a>
+					</li>
+					<li classNames='w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+						<a href="https://github.com/rmorbey" className='flex justify-between items-center w-full text-gray-300 px-4'>
+						Github <FaGithub size={30} />
+						</a>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+	
 	)
 }
+
 export default Navbar
