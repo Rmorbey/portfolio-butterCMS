95e68584fe767460f2fda8ceec91f2d531861d53
Russell <russellmorbey@hotmail.co.uk>
2025-07-04 22:11:57 +0100

    finished navbar and social icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import {
 	FaLinkedin,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -88,6 +87,11 @@ const Navbar = () => {
 					<li classNames='w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
 						<a href="https://github.com/rmorbey" className='flex justify-between items-center w-full text-gray-300 px-4'>
 						Github <FaGithub size={30} />
+						</a>
+					</li>
+					<li classNames='w-40 h-14 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+						<a href="mailto:russellmorbey@hotmail.co.uk" className='flex justify-between items-center w-full text-gray-300 px-4'>
+						Email <HiOutlineMail size={30} />
 						</a>
 					</li>
 				</ul>
