b6331cfc7ce7b69e3034b076b89a834cf3a62d3c
Russell <russellmorbey@hotmail.co.uk>
2025-07-07 17:07:50 +0100

    Api call using axios

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Works from './components/Works';
 import Other from './components/Other';
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const readtoken = '06aefcf9318265fb82a0bd922ead6f2fa1d0cfdb'
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      axios.get(`https://api.buttercms.com/v2/pages/portfolio/a-portfolio-site?auth_token=${readtoken}`).then(res => {
+        setData(res.data.data.fields.my_personal_portfolio);
+      }).catch(err => {
+        console.log(err);
+      })
+    }
+    getData();
+  }, []);
   return (
     <div >
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Works />
+      <Home content={data[0]}/>
+      <About content={data[1]}/>
+      <Skills content={data[2]}/>
+      <Works content={data[3]}/>
       <Contact />
       <Other />
     </div>
